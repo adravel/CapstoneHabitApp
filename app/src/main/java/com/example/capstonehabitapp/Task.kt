e52a7870b92a946c226d4afe284a2dd691580642
Arrafly A Dori <arrafly.ad@gmail.com>
2022-03-17 22:32:38 +0700

    Update Task data class

--- app/src/main/java/com/example/capstonehabitapp/Task.kt
+++ app/src/main/java/com/example/capstonehabitapp/Task.kt
@@ -1,9 +1,20 @@
 package com.example.capstonehabitapp
 
+import java.util.*
+
 data class Task(
     var title: String = "",
+    var category: String = "",
+    var difficulty: Long = -1,
     var area: String = "",
     var startTimeLimit: String = "",
     var finishTimeLimit: String = "",
-    var status: Long = -1
+    var status: Long = -1,
+    var detail: String = "",
+    var gradePoints: Long = -1,
+    var notes: String = "",
+    var createTime: Date? = null,
+    var startTime: Date? = null,
+    var finishTime: Date? = null,
+    var photoUri: String = ""
 )
