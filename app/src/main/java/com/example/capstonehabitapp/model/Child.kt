be0382611b219bb7213ac680307e8d124e4e0943
Arrafly A Dori <arrafly.ad@gmail.com>
2022-04-01 16:23:08 +0700

    Fix no setter/field warning in Child data class

--- app/src/main/java/com/example/capstonehabitapp/model/Child.kt
+++ app/src/main/java/com/example/capstonehabitapp/model/Child.kt
@@ -1,5 +1,6 @@
 package com.example.capstonehabitapp.model
 
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentId
 import java.util.*
 
@@ -7,11 +8,10 @@ data class Child(
     @DocumentId
     var id: String = "",
     var name: String = "",
-    var isMale: Boolean = true,
     var age: Long = -1,
     var totalPoints: Long = -1,
     var currentPoints: Long = -1,
     var level: Long = -1,
     var badge: Long = -1,
-    var createTime: Date? = null
+    var timeCreated: Timestamp? = null
 )
