566d4f857733cdcb1a9f22684e5380b9707e7385
Arrafly A Dori <arrafly.ad@gmail.com>
2022-04-09 22:17:19 +0700

    Add function to add new task to Firestore

--- app/src/main/java/com/example/capstonehabitapp/ui/TaskCreationFragment.kt
+++ app/src/main/java/com/example/capstonehabitapp/ui/TaskCreationFragment.kt
@@ -1,17 +1,24 @@
 package com.example.capstonehabitapp.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.example.capstonehabitapp.R
 import com.example.capstonehabitapp.databinding.FragmentTaskCreationBinding
+import com.example.capstonehabitapp.viewmodel.TaskCreationViewModel
 
 class TaskCreationFragment : Fragment() {
 
     private var _binding: FragmentTaskCreationBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var viewModel: TaskCreationViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,12 +28,56 @@ class TaskCreationFragment : Fragment() {
         // Set toolbar title
         activity?.title = getString(R.string.create_task)
 
+        // Initialize the ViewModel
+        viewModel = ViewModelProvider(this)[TaskCreationViewModel::class.java]
+
         _binding = FragmentTaskCreationBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            // Disable button by default
+            createTaskButton.isEnabled = false
+
+            // Set TextChangedListener for task title, area, and difficulty EditTexts
+            val editTexts = listOf(taskTitleEditText, taskAreaEditText, taskDifficultyEditText)
+            for (editText in editTexts) {
+                editText.addTextChangedListener(object : TextWatcher {
+                    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                        val et1 = taskTitleEditText.text.toString().trim()
+                        val et2 = taskAreaEditText.text.toString().trim()
+                        val et3 = taskDifficultyEditText.text.toString().trim()
+
+                        // Enable button if the EditTexts is not empty
+                        createTaskButton.isEnabled = et1.isNotEmpty()
+                                && et2.isNotEmpty()
+                                && et3.isNotEmpty()
+                    }
+
+                    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+                    override fun afterTextChanged(p0: Editable?) {}
+                })
+            }
+
+            createTaskButton.setOnClickListener {
+                val title = taskTitleEditText.text.toString()
+                val area = taskAreaEditText.text.toString()
+                val difficulty = taskDifficultyEditText.text.toString().toInt()
+                val startTimeLimit = taskStartTimeLimitEditText.text.toString()
+                val finishTimeLimit = taskFinishTimeLimitEditText.text.toString()
+                val detail = taskDetailEditText.text.toString()
+
+                // add new task data to Firestore and get the ID
+                val id = viewModel.addTaskToFirebase(title, area, difficulty, startTimeLimit, finishTimeLimit, detail)
+
+                // Navigate to task detail page
+                val action = TaskCreationFragmentDirections.actionTaskCreationFragmentToTaskDetailFragment(id)
+                view.findNavController().navigate(action)
+            }
+        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/capstonehabitapp/viewmodel/TaskCreationViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.capstonehabitapp.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.lang.Exception
+
+class TaskCreationViewModel: ViewModel() {
+    private val testParentId = "2p8at5eicReHAP1P4zDu"
+    private val parentDocRef = Firebase.firestore.collection("parents").document(testParentId)
+
+    // Add new task to Firestore and return its ID
+    fun addTaskToFirebase(
+        title: String,
+        area: String,
+        difficulty: Int,
+        startTimeLimit: String,
+        finishTimeLimit: String,
+        detail: String): String {
+
+        // Generate empty document reference
+        val taskDocRef = parentDocRef.collection("tasks").document()
+
+        val newTask = hashMapOf(
+            "title" to title,
+            "area" to area,
+            "difficulty" to difficulty,
+            "startTimeLimit" to startTimeLimit,
+            "finishTimeLimit" to finishTimeLimit,
+            "detail" to detail,
+            "status" to 0,
+            "timeCreated" to FieldValue.serverTimestamp()
+        )
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                // Send the data using Firestore set() method
+                taskDocRef.set(newTask).await()
+
+            } catch (e: Exception) {
+                e.message?.let { Log.e("TaskCreation", it) }
+            }
+        }
+        return taskDocRef.id
+    }
+}
