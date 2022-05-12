20edae696dbfa4f24b71023ad2b181565315c9ef
Arrafly A Dori <arrafly.ad@gmail.com>
2022-05-12 15:43:28 +0700

    Add method to login user

--- app/src/main/java/com/example/capstonehabitapp/ui/LoginFragment.kt
+++ app/src/main/java/com/example/capstonehabitapp/ui/LoginFragment.kt
@@ -3,19 +3,26 @@ package com.example.capstonehabitapp.ui
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.example.capstonehabitapp.R
 import com.example.capstonehabitapp.databinding.FragmentLoginBinding
+import com.example.capstonehabitapp.util.Response
+import com.example.capstonehabitapp.viewmodel.LoginViewModel
 
 class LoginFragment: Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -53,8 +60,35 @@ class LoginFragment: Fragment() {
 
             // Set login button onCLickListener
             loginButton.setOnClickListener {
-                // Navigate to role selection page
-                findNavController().navigate(R.id.roleSelectionFragment)
+                val email = binding.emailEditText.text.toString().lowercase()
+                val password = binding.passwordEditText.text.toString()
+
+                // Call the method to register user
+                viewModel.loginUser(email, password)
+            }
+
+            // Observe user LiveData in ViewModel
+            // This value determines whether user login is successful or not
+            viewModel.user.observe(viewLifecycleOwner) { response ->
+                when (response) {
+                    is Response.Loading -> {}
+                    is Response.Success -> {
+                        Toast.makeText(context, getString(R.string.login_success), Toast.LENGTH_SHORT).show()
+
+                        // Build navigation options to pop all auth fragments before navigating
+                        val navOptions = NavOptions.Builder()
+                            .setPopUpTo(R.id.welcomeFragment, true)
+                            .build()
+
+                        // Navigate to role selection page
+                        val action = LoginFragmentDirections.actionLoginFragmentToRoleSelectionFragment()
+                        findNavController().navigate(action, navOptions)
+                    }
+                    is Response.Failure -> {
+                        Log.e("Login", response.message)
+                        Toast.makeText(context, getString(R.string.request_failed), Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
 
             // Set register option text onClickListener
--- app/src/main/java/com/example/capstonehabitapp/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/capstonehabitapp/viewmodel/LoginViewModel.kt
@@ -12,20 +12,20 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
-class RegisterViewModel: ViewModel() {
+class LoginViewModel: ViewModel() {
     private val auth = Firebase.auth
 
     private val _user: MutableLiveData<Response<FirebaseUser>> = MutableLiveData()
     val user: LiveData<Response<FirebaseUser>> = _user
 
-    // Create a new user in Firebase
-    fun registerUser(email: String, password: String) {
+    // Log user into Firebase Auth
+    fun loginUser(email: String, password: String) {
         _user.postValue(Response.Loading())
 
         CoroutineScope(Dispatchers.IO).launch {
             try {
                 // Create new user
-                auth.createUserWithEmailAndPassword(email, password).await()
+                auth.signInWithEmailAndPassword(email, password).await()
                 val currentUser = auth.currentUser!!
 
                 _user.postValue(Response.Success(currentUser))
