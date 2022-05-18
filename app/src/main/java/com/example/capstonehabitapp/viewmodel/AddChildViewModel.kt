3b6cc484f5fe0c575b1d05add11b60ddaaf93546
Arrafly A Dori <arrafly.ad@gmail.com>
2022-05-18 13:45:09 +0700

    Add houses and tools data in batch to Firestore when adding new child

--- app/src/main/java/com/example/capstonehabitapp/viewmodel/AddChildViewModel.kt
+++ app/src/main/java/com/example/capstonehabitapp/viewmodel/AddChildViewModel.kt
@@ -3,6 +3,8 @@ package com.example.capstonehabitapp.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.capstonehabitapp.model.House
+import com.example.capstonehabitapp.model.Tool
 import com.example.capstonehabitapp.util.Response
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FieldValue
@@ -22,24 +24,40 @@ class AddChildViewModel: ViewModel() {
     private val _childResponse: MutableLiveData<Response<Int>> = MutableLiveData()
     val childResponse: LiveData<Response<Int>> = _childResponse
 
-    // Create new child document in Firestore
+    // Create new child, tool, and house documents simultaneously
+    // using Firestore batch writes
     fun addChildToFirebase(name: String, isMale: Boolean) {
         _childResponse.postValue(Response.Loading())
 
         CoroutineScope(Dispatchers.IO).launch {
             try {
-                val child = hashMapOf(
-                    "name" to name,
-                    "isMale" to isMale,
-                    "totalPoints" to 0,
-                    "cash" to 0,
-                    "level" to 1,
-                    "badge" to 0,
-                    "timeCreated" to FieldValue.serverTimestamp()
-                )
+                db.runBatch { batch ->
+                    val newChild = hashMapOf(
+                        "name" to name,
+                        "isMale" to isMale,
+                        "totalPoints" to 0,
+                        "cash" to 0,
+                        "level" to 1,
+                        "badge" to 0,
+                        "timeCreated" to FieldValue.serverTimestamp()
+                    )
 
-                // Add new child document to Firestore "children" collection
-                parentDocRef.collection("children").add(child).await()
+                    // Create a new child document
+                    val newChildDocRef = parentDocRef.collection("children").document()
+                    batch.set(newChildDocRef, newChild)
+
+                    // Add the house documents
+                    for (newHouse in newHouses) {
+                        val newHouseDocRef = newChildDocRef.collection("houses").document()
+                        batch.set(newHouseDocRef, newHouse)
+                    }
+
+                    // Add the tool documents
+                    for (newTool in newTools) {
+                        val newToolDocRef = newChildDocRef.collection("tools").document()
+                        batch.set(newToolDocRef, newTool)
+                    }
+                }.await()
 
                 _childResponse.postValue(Response.Success(1))
 
@@ -48,4 +66,54 @@ class AddChildViewModel: ViewModel() {
             }
         }
     }
+
+    // List of house data to be added to Firestore when creating a new child document
+    private val newHouses = listOf(
+        House(
+            "",
+            1,
+            1,
+            150,
+            "Bangsal Kencono",
+            "Jawa",
+            "",
+            150,
+            ""
+        ),
+        House(
+            "",
+            2,
+            0,
+            250,
+            "Bubung Lima",
+            "Sumatra",
+            "",
+            250,
+            ""
+        )
+    )
+
+    // List of tool data to be added to Firestore when creating a new child document
+    private val newTools = listOf(
+        Tool(
+            "",
+            1,
+            true,
+            false,
+            "Meriam",
+            7,
+            150,
+            ""
+        ),
+        Tool(
+            "",
+            2,
+            true,
+            false,
+            "Bom",
+            10,
+            200,
+            ""
+        )
+    )
 }
